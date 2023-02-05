6731133849145523bc822103812bac27694299e5
Zenax <124360636+Zenax0001@users.noreply.github.com>
2023-02-05 21:54:35 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# j_hud
+
+### Installation
+1) Download the latest version in the "code" tab on GitHub.
+2) Drag & drop the folder into your `resources` server folder.
+3) Add `start j_hud` to your server.cfg
+4) Reboot your server.
+
+![](https://img001.prntscr.com/file/img001/r3tEIb5QSK6cmyh2zvU4Mg.png)
---
+++ j_hud/__resource.lua
@@ -0,0 +1,19 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+client_scripts {
+    "client.lua"
+}
+
+server_scripts {
+    "HTML/lib.lua"
+}
+
+ui_page "HTML/index.html"
+
+files {
+    "HTML/fonts/*",
+    "HTML/images/*",
+    "HTML/design.css",
+    "HTML/index.html",
+    "HTML/script.js",
+}
